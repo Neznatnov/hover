ac4d46a1d2030778d738c68afc85ce3f632c2c16
Veronika Iatckaia <neznatnov@gmail.com>
2023-04-23 14:48:07 +0200

    nothing changed

---
+++ src/test/java/com/neznatnov/MainTest.java
@@ -0,0 +1,21 @@
+package com.neznatnov;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+class MainTest {
+
+    @Test
+    void hoverTest(){
+        open("https://github.com/");
+
+        $("header").$(byText("Solutions")).hover();
+        $(By.cssSelector("a[href='/enterprise']")).click();
+        $("h1").shouldHave(text("Build like the best"));
+    }
+}
